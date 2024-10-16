accb798f1638234c8daa36dd236a8aa58435e6c9
Imperial 07 83 65 42 32 <emmanuel.levard@edu.univ-fcomte.fr>
2024-10-16 10:57:14 +0200

    déplacement du js dans le fichier js

--- economie/economie.js
+++ economie/economie.js
@@ -11,3 +11,49 @@ function swap_image(index_change){
     }
     image.src=array_name[pos];
 }
+
+function load(url, callback) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+        callback(xhr.response);
+        }
+    };
+
+    xhr.open("GET", "https://quote.cnbc.com/quote-html-webservice/restQuote/symbolType/symbol?symbols=ORA-FR&requestMethod=itv&noform=1&partnerId=2&fund=1&exthrs=1&output=json&events=1",true);
+
+    xhr.send("");
+    }
+    function inserter(result){
+        let rs_js=JSON.parse(result);
+        //document.write('<div>Price of Orange : Close -  ' + result+ '</div>');
+        
+        const element = document.getElementById("api_results_holder");
+        let vls=rs_js.FormattedQuoteResult.FormattedQuote[0];
+        let nom=document.createElement("p");
+        let valeur_actuelle=document.createElement("p");
+        let taux_dividentes=document.createElement("a");
+        let plage_annee=document.createElement("p");
+        let place_boursiere_principale=document.createElement("p");
+        let ratio_pe=document.createElement("p");
+
+        nom.innerHTML="Nom de bourse: "+vls.symbol;
+        valeur_actuelle.innerHTML="Dernier prix d'action : "+vls.last;
+        taux_dividentes.innerHTML="Rendement (dividendes): "+vls.dividend;
+        plage_annee.innerHTML="Plage sur l'année: "+vls.yrloprice+" - "+ vls.yrhiprice;
+        place_boursiere_principale.innerHTML="Place Boursière principale: "+vls.exchange;
+        ratio_pe.innerHTML="Ratio cours/bénéfices: "+vls.pe;
+
+        taux_dividentes.href="https://fr.wikipedia.org/wiki/Dividende";
+
+
+        element.appendChild(nom);
+        element.appendChild(valeur_actuelle);
+        element.appendChild(taux_dividentes);
+        element.appendChild(plage_annee);
+        element.appendChild(place_boursiere_principale);
+        element.appendChild(ratio_pe);
+        //document.write('<div>Price of Orange : Close -  ' + rs_js.FormattedQuoteResult.FormattedQuote[0].symbol+ '</div>');
+    };
+    load("",inserter);
